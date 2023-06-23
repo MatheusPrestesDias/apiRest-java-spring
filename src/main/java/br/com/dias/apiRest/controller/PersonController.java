d72e418bfa9f44a704729efc631737fc5be17ddf
MatheusPrestesDias <matheuspdias@outlook.com.br>
2023-06-22 21:04:31 -0300

    Refatorado controller e service;

--- src/main/java/br/com/dias/apiRest/controller/PersonController.java
+++ src/main/java/br/com/dias/apiRest/controller/PersonController.java
@@ -1,9 +1,8 @@
 package br.com.dias.apiRest.controller;
 
-import br.com.dias.apiRest.model.Person;
+import br.com.dias.apiRest.data.dto.v1.PersonDTO;
 import br.com.dias.apiRest.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,23 +17,23 @@ public class PersonController {
     private PersonService personService;
 
     @GetMapping("/{id}")
-    public Person findById(@PathVariable(value = "id") Long id) {
+    public PersonDTO findById(@PathVariable(value = "id") Long id) {
         return personService.findById(id);
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Person> findAll() {
+    public List<PersonDTO> findAll() {
         return personService.findAll();
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person create(@RequestBody Person person) {
-        return personService.create(person);
+    public PersonDTO create(@RequestBody PersonDTO personDTO) {
+        return personService.create(personDTO);
     }
 
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person update(@RequestBody Person person) {
-        return personService.update(person);
+    public PersonDTO update(@RequestBody PersonDTO personDTO) {
+        return personService.update(personDTO);
     }
 
     @DeleteMapping(value = "/{id}")
--- src/main/java/br/com/dias/apiRest/service/PersonService.java
+++ src/main/java/br/com/dias/apiRest/service/PersonService.java
@@ -1,12 +1,15 @@
 package br.com.dias.apiRest.service;
 
+import br.com.dias.apiRest.data.dto.v1.PersonDTO;
 import br.com.dias.apiRest.exceptions.ResourceNotFoundException;
 import br.com.dias.apiRest.model.Person;
 import br.com.dias.apiRest.repository.PersonRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
@@ -14,32 +17,43 @@ public class PersonService {
     @Autowired
     PersonRepository personRepository;
 
-    public Person findById(Long id) {
-        return personRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public PersonDTO findById(Long id) {
+        Person person = personRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID."));
+        return modelMapper.map(person, PersonDTO.class);
     }
 
-    public List<Person> findAll() {
-        return personRepository.findAll();
+    public List<PersonDTO> findAll() {
+        List<Person> persons = personRepository.findAll();
+        return persons.stream()
+                .map(person -> modelMapper.map(person, PersonDTO.class))
+                .collect(Collectors.toList());
     }
 
-    public Person create(Person person) {
-        return personRepository.save(person);
+    public PersonDTO create(PersonDTO person) {
+        Person personEntity = modelMapper.map(person, Person.class);
+        Person savedPerson = personRepository.save(personEntity);
+        return modelMapper.map(savedPerson, PersonDTO.class);
+        // return modelMapper.map(personRepository.save(personEntity), PersonDTO.class);
     }
 
-    public Person update(Person person) {
+    public PersonDTO update(PersonDTO person) {
         Person entity = personRepository.findById(person.getId())
-                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID."));
         entity.setFirstName(person.getFirstName());
         entity.setLastName(person.getLastName());
         entity.setAddress(person.getAddress());
         entity.setGender(person.getGender());
-        return personRepository.save(entity);
+        personRepository.save(entity);
+        return modelMapper.map(entity, PersonDTO.class);
     }
 
     public void delete(Long id) {
         Person person = personRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID."));
         personRepository.delete(person);
     }
 }
