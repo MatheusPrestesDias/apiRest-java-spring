edcb4f3fdc0cdef716effadeef7f3a4ca88c12ed
MatheusPrestesDias <matheuspdias@outlook.com.br>
2023-06-24 17:47:09 -0300

    adicionado flyway e migrations para criação do banco;

---
+++ src/main/resources/db/migration/V2__populate_table_person.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+     (1, 'SRS, MG', 'José', 'Male', 'Silva teste2'),
+     (5, 'SP, SP', 'Maria', 'Female', 'Silva'),
+     (6, 'SP, SP', 'Maria', 'Female', 'Silva'),
+     (7, 'SP, SP', 'Maria', 'Female', 'Silva'),
+     (8, 'SP, SP', 'Nelson', 'Female', 'José');
+
+
