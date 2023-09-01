a0679525d81e2067153cda44f24f37beadfc9494
MatheusPrestesDias <matheuspdias@outlook.com.br>
2023-09-01 17:47:24 -0300

    novas migrations com as tabelas para autenticação;

---
+++ src/main/resources/db/migration/V8__populate_table_users.sql
@@ -0,0 +1,10 @@
+﻿
+INSERT INTO `user` (`user_name`, `full_name`, `password`,
+                    `account_non_expired`, `account_non_locked`,
+                     `credentials_non_expired`, `enabled`)
+VALUES ('matheus', 'Matheus Dias',
+        'bdf65a288664968614083de861b294eb5859431f1c95332687dc21d6ed0ce38e3a42322bfbbf68e5',
+        b'1', b'1', b'1', b'1'),
+       ('carlos', 'Carlos Dias',
+        'bdf65a288664968614083de861b294eb5859431f1c95332687dc21d6ed0ce38e3a42322bfbbf68e5',
+        b'1', b'1', b'1', b'1');
